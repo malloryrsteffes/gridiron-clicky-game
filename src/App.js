b06f52841539159f8a2e781808707c0db44e9356
Mal <malrsteffes@gmail.com>
2019-10-28 22:21:31 -0400

    source photos added as a final push for the night

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-
+import Main from "./components/Main";
 
 function App() {
   return (
     <div>
      <Navbar />
      <Header />
+     <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Main.css";
+
+function Main(){
+    return (
+        <main className="container">
+            <div className="click-item"></div>
+            <div className="click-item"></div>
+            <div className="click-item"></div>
+            <div className="click-item"></div>
+            <div className="click-item"></div>
+            <div className="click-item"></div>
+            <div className="click-item"></div>
+            <div className="click-item"></div>
+            <div className="click-item"></div>
+            <div className="click-item"></div>
+            <div className="click-item"></div>
+            <div className="click-item"></div>
+
+        </main>
+    )
+}
+
+export default Main;
